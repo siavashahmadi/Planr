989a28b690bbccce489b59552580d8af3280a207
Siavash Ahmadi <sahmadi4@student.gsu.edu>
2023-04-06 23:24:23 -0400

    underline on calendar

--- src/components/Calendar.tsx
+++ src/components/Calendar.tsx
@@ -1,8 +1,8 @@
 const Calendar = () => {
   return (
-    <div className="rounded-md mt-3 p-2 bg-[#ffecc6] shadow-md h-screen flex flex-row justify-between">
-      <div className="">
-        <h1 className="flex text-lg font-semibold p-2">Calendar</h1>
+    <div className="rounded-md mt-3 p-2 bg-[#ffecc6] shadow-md h-screen flex flex-col justify-between">
+      <div className="border-b border-[#ffd788]">
+        <h1 className="flex text-lg font-semibold p-1">Calendar</h1>
       </div>
     </div>
   );
