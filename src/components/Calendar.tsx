4916589356eb7abcebd9d0514bfd1f0ece93b509
Dan4422 <sdlee095@gmail.com>
2023-04-11 21:33:51 -0400

    implement month change

--- src/components/Calendar.tsx
+++ src/components/Calendar.tsx
@@ -1,27 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import Day from "./Day";
+import Month from "./Month";
 
 const Calendar = () => {
   const monthsDays = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+  const [month, setMonth] = useState(12)
 
   return (
-    <div className="grid grid-cols-7 mt-3 w-full lg:w-3/4 bg-white rounded-xl">
-      <div className="p-2 text-[#838383]">Sunday</div>
-      <div className="p-2 text-[#838383]">Monday</div>
-      <div className="p-2 text-[#838383]">Tuesday</div>
-      <div className="p-2 text-[#838383]">Wednesday</div>
-      <div className="p-2 text-[#838383]">Thursday</div>
-      <div className="p-2 text-[#838383]">Friday</div>
-      <div className="p-2 text-[#838383]">Saturday</div>
-      {monthsDays.map((monthDays, monthIndex) => {
-        return (
-          <React.Fragment key={monthIndex}>
-            {Array.from({ length: monthDays }, (_, dayIndex) => (
-              <Day key={dayIndex} day={dayIndex + 1} month={monthIndex + 1} />
-            ))}
-          </React.Fragment>
-        );
-      })}
+    <div className="flex flex-col">
+      <div className="w-72">
+        <select value={month} onChange={e => setMonth(Number(e.target.value))}>
+          <option value={12} disabled>Choose a month</option>
+          <option value={0}>January</option>
+          <option value={1}>February</option>
+          <option value={2}>March</option>
+          <option value={3}>April</option>
+          <option value={4}>May</option>
+          <option value={5}>June</option>
+          <option value={6}>July</option>
+          <option value={7}>August</option>
+          <option value={8}>September</option>
+          <option value={9}>October</option>
+          <option value={10}>November</option>
+          <option value={11}>December</option>
+        </select>
+      </div>
+      <Month monthIndex={month} days={monthsDays[month]} />
     </div>
   );
 };
